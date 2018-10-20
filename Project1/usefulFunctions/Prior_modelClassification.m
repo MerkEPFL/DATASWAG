489e2bdf1a0a258c7829f319321f33b59a8bcce7
FrancescaLuongo <francesca.luongo@epfl.ch>
2018-10-20 19:29:51 +0200

    miaou

---
+++ Project1/usefulFunctions/Prior_modelClassification.m
@@ -0,0 +1,13 @@
+function [classifier, yhat, Loss] = Prior_modelClassification(features, labels, discrimtype)
+%utile pour la partie 1 du guidesheet2
+
+
+classifier = fitcdiscr(features, labels, 'DiscrimType', discrimtype,'Prior', 'uniform');
+yhat = predict(classifier, features); %Predict labels using discriminant analysis classification model, ce qu'il faudra tester
+
+
+Loss = loss(classifier, features, labels); % is an inherited method to find the Classification error, 
+%L = loss(obj,X,Y) returns the classification loss, which is a scalar representing how well obj classifies the data in X, when Y contains the true classifications
+
+
+end
