b74ed2b94242db8dd54a6707e252a0f34c72e5bd
FrancescaLuongo <francesca.luongo@epfl.ch>
2018-10-20 19:40:32 +0200

    :=)

--- Project1/Guidesheet2.m
+++ Project1/Guidesheet2.m
@@ -9,13 +9,24 @@ load('testSet.mat')
 [~,~,L2] = modelClassification(trainData, trainLabels,'diagLinear')
 [~,~,L3] = modelClassification(trainData, trainLabels,'diagQuadratic')
 
-
-
 plot(L2,'mo') %3rd
 hold on;
 plot(L1,'go') %best
 hold on;
 plot(L3,'co') %2nd
+hold on;
+
+% with prior
+
+[~,~,L1p] = Prior_modelClassification(trainData, trainLabels,'Linear')
+[~,~,L2p] = Prior_modelClassification(trainData, trainLabels,'diagLinear')
+[~,~,L3p] = Prior_modelClassification(trainData, trainLabels,'diagQuadratic')
+
+plot(2, L2p,'ro') %2rd
+hold on;
+plot(2,L1p,'ko') %1st
+hold on;
+plot(2,L3p,'bo') %2nd
 hold on;
 
 
@@ -29,22 +40,6 @@ hold on;
 % loss =loss(classifier, trainData, trainLabels)
 % 
 
-%% with prior
-
-[~,~,L1] = Prior_modelClassification(trainData, trainLabels,'Linear')
-[~,~,L2] = Prior_modelClassification(trainData, trainLabels,'diagLinear')
-[~,~,L3] = Prior_modelClassification(trainData, trainLabels,'diagQuadratic')
-
-plot(L2,'mo') %3rd
-hold on;
-plot(L1,'go') %best
-hold on;
-plot(L3,'co') %2nd
-hold on;
-
-
-
-%%
 
 
 
@@ -52,4 +47,9 @@ hold on;
 % fonctionne pas car: "You cannot use 'quadratic' type because one or more classes have singular covariance matrices."
 
 [~,~,L4] = modelClassification(trainData, trainLabels,'Quadratic')  
+plot(L4,'o')
+hold on;
+
+% avec prior aussi
+[~,~,L4p] = Prior_modelClassification(trainData, trainLabels,'Quadratic')
 plot(L4,'o')
